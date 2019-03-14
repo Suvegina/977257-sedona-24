0868b0575ccd773f37294798465337c4eea4dfef
Надежда <nadezhda@legeart.pro>
2019-03-14 05:04:26 +0300

    Правки ч.4

--- js/min-script.js
+++ js/min-script.js
@@ -1,10 +1,10 @@
-// var button-link = document.queryselector (".appointment-button");
-// var popup = document.queryselector (".pop-up");
-// // var close = document.queryselector (".appointment-button");
+var button-link = document.queryselector (".appointment-button");
+var popup = document.queryselector (".pop-up");
+// var close = document.queryselector (".appointment-button");
  
-// button-link.addEventListener("click", function (evt) {
-// 	evt.prevenDefault();
-// 	console.log("Клик по кнопке поиск гостиницы");
-// 	popup.classlist.toggle("modal-show");
-// });
+button-link.addEventListener("click", function (evt) {
+	evt.prevenDefault();
+	console.log("Клик по кнопке поиск гостиницы");
+	popup.classlist.toggle("modal-show");
+});
 
