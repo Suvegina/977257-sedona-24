52e4112ee3d67089626efd22a96ab4a16a212880
Надежда <nadezhda@legeart.pro>
2019-03-15 09:22:03 +0300

    /

--- js/min-script.js
+++ js/min-script.js
@@ -1,10 +1,8 @@
-var button-link = document.queryselector (".appointment-button");
-var popup = document.queryselector (".pop-up");
-// var close = document.queryselector (".appointment-button");
- 
-button-link.addEventListener("click", function (evt) {
-	evt.prevenDefault();
-	console.log("Клик по кнопке поиск гостиницы");
-	popup.classlist.toggle("modal-show");
+var buttonLink = document.querySelector (".appointment-button");
+var popup = document.querySelector (".modal");
+
+buttonLink.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.toggle("modal-show");
 });
 
